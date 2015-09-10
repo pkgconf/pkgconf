41cb57e9cd1d88e52d27ca61ad3400a749eeacb3
Dan Kegel <dank@oblong.com>
2015-09-10 16:35:06 -0700

    Add unit test for argvsplit

---
+++ libpkgconf/tests/argvsplit-test.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <assert.h>
+
+#include "../libpkgconf.h"
+
+void test_simple()
+{
+	int argc;
+	char **argv;
+
+	pkgconf_argv_split("A B", &argc, &argv);
+	assert(argc == 2);
+	assert(!strcmp(argv[0], "A"));
+	assert(!strcmp(argv[1], "B"));
+	pkgconf_argv_free(argv);
+}
+
+void test_escaped()
+{
+	int argc;
+	char **argv;
+
+	pkgconf_argv_split("A\\ B", &argc, &argv);
+	assert(argc == 1);
+	assert(!strcmp(argv[0], "A\\ B"));
+	pkgconf_argv_free(argv);
+}
+
+int main(int argc, char **argv)
+{
+	(void) argc; (void) argv;
+	test_simple();
+	test_escaped();
+}
