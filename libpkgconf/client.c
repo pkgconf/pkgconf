1ece862e8763d2953274710633542b5509c0b33a
William Pitcock <nenolod@dereferenced.org>
2016-12-30 02:35:36 -0600

    libpkgconf: client: some toolchains have C_INCLUDE_PATH and CPLUS_INCLUDE_PATH, treat them as PKG_CONFIG_SYSTEM_INCLUDE_PATH.

--- libpkgconf/client.c
+++ libpkgconf/client.c
@@ -56,6 +56,8 @@ pkgconf_client_init(pkgconf_client_t *client, pkgconf_error_handler_func_t error
 
 	pkgconf_path_build_from_environ("PKG_CONFIG_SYSTEM_LIBRARY_PATH", SYSTEM_LIBDIR, &client->filter_libdirs);
 	pkgconf_path_build_from_environ("PKG_CONFIG_SYSTEM_INCLUDE_PATH", SYSTEM_INCLUDEDIR, &client->filter_includedirs);
+	pkgconf_path_build_from_environ("C_INCLUDE_PATH", NULL, &client->filter_includedirs);
+	pkgconf_path_build_from_environ("CPLUS_INCLUDE_PATH", NULL, &client->filter_includedirs);
 }
 
 /*
