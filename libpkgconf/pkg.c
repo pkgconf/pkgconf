7556285d49f329108bd7a64d8b724843f9815fc0
Baptiste Daroussin <bapt@FreeBSD.org>
2017-01-22 11:25:05 +0100

    Actually fix the regression introduced in 7b39c38

--- libpkgconf/pkg.c
+++ libpkgconf/pkg.c
@@ -1336,6 +1336,7 @@ pkgconf_pkg_traverse(pkgconf_client_t *client,
 	void *data,
 	int maxdepth)
 {
+	unsigned int saved_flags = client->flags;
 	unsigned int eflags = PKGCONF_PKG_ERRF_OK;
 
 	if (maxdepth == 0)
@@ -1346,6 +1347,7 @@ pkgconf_pkg_traverse(pkgconf_client_t *client,
 		if (func != NULL)
 			func(client, root, data);
 	}
+	client->flags &= ~PKGCONF_PKG_PKGF_SKIP_ROOT_VIRTUAL;
 
 	if (!(client->flags & PKGCONF_PKG_PKGF_SKIP_CONFLICTS))
 	{
@@ -1368,6 +1370,7 @@ pkgconf_pkg_traverse(pkgconf_client_t *client,
 		if (eflags != PKGCONF_PKG_ERRF_OK)
 			return eflags;
 	}
+	client->flags = saved_flags;
 
 	return eflags;
 }
