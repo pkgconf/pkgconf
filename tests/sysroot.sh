4e50938e789e35dabd9b8f3b0401f6c4f3309eed
Baptiste Daroussin <bapt@FreeBSD.org>
2017-10-11 17:01:19 +0200

    tests: add a test case for #146

--- tests/sysroot.sh
+++ tests/sysroot.sh
@@ -4,7 +4,17 @@
 
 tests_init \
 	cflags \
-	variable
+	variable \
+	do_not_eat_slash \
+
+do_not_eat_slash_body()
+{
+	export PKG_CONFIG_PATH="${selfdir}/lib1"
+	export PKG_CONFIG_SYSROOT_DIR="/"
+	atf_check \
+		-o inline:"-fPIC -I/test/include/foo \n" \
+		pkgconf --cflags baz
+}
 
 cflags_body()
 {
