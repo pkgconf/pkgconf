3384f59d23b55cec48ab9cfff9086c3c2ea087c5
Patrick Griffis <tingping@tingping.se>
2018-03-06 20:34:00 -0500

    meson: Fix defining PKGCONF_API

--- libpkgconf/libpkgconf.h
+++ libpkgconf/libpkgconf.h
@@ -20,9 +20,9 @@
 #include <stdarg.h>
 #include <stddef.h>
 #include <stdbool.h>
+#include <libpkgconf/libpkgconf-api.h>
 #include <libpkgconf/iter.h>
 #include <libpkgconf/bsdstubs.h>
-#include <libpkgconf/libpkgconf-api.h>
 
 #ifdef __cplusplus
 extern "C" {
