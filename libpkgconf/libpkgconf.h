c862e030cf83447f679e4f49876f5298f0fc9691
Ariadne Conill <ariadne@dereferenced.org>
2019-07-12 06:53:37 -0500

    pkgconf 1.6.3.

--- libpkgconf/libpkgconf.h
+++ libpkgconf/libpkgconf.h
@@ -78,8 +78,8 @@ typedef struct pkgconf_cross_personality_ pkgconf_cross_personality_t;
 #define PKGCONF_FOREACH_LIST_ENTRY_REVERSE(tail, value) \
 	for ((value) = (tail); (value) != NULL; (value) = (value)->prev)
 
-#define LIBPKGCONF_VERSION	10602
-#define LIBPKGCONF_VERSION_STR	"1.6.2"
+#define LIBPKGCONF_VERSION	10603
+#define LIBPKGCONF_VERSION_STR	"1.6.3"
 
 struct pkgconf_fragment_ {
 	pkgconf_node_t iter;
