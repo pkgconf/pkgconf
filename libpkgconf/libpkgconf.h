dd57abfe9f53ae6822e5a736e3b1692953a682fc
Ariadne Conill <ariadne@dereferenced.org>
2020-05-24 14:44:01 -0600

    pkgconf 1.7.0.

--- libpkgconf/libpkgconf.h
+++ libpkgconf/libpkgconf.h
@@ -78,8 +78,8 @@ typedef struct pkgconf_cross_personality_ pkgconf_cross_personality_t;
 #define PKGCONF_FOREACH_LIST_ENTRY_REVERSE(tail, value) \
 	for ((value) = (tail); (value) != NULL; (value) = (value)->prev)
 
-#define LIBPKGCONF_VERSION	10990
-#define LIBPKGCONF_VERSION_STR	"1.9.90"
+#define LIBPKGCONF_VERSION	10700
+#define LIBPKGCONF_VERSION_STR	"1.7.0"
 
 struct pkgconf_fragment_ {
 	pkgconf_node_t iter;
