7fa632a6582014cf98d1e198a3ba735b26f38161
Ariadne Conill <ariadne@dereferenced.org>
2022-06-26 19:34:54 +0000

    tests: sysroot: add additional tests

--- tests/sysroot.sh
+++ tests/sysroot.sh
@@ -6,6 +6,7 @@ tests_init \
 	cflags \
 	variable \
 	do_not_eat_slash \
+	do_not_duplicate_sysroot_dir
 
 do_not_eat_slash_body()
 {
@@ -35,4 +36,28 @@ variable_body()
 	atf_check \
 		-o inline:"${SYSROOT_DIR}/test/include\n" \
 		pkgconf --variable=includedir foo
+}
+
+do_not_duplicate_sysroot_dir_body()
+{
+	export PKG_CONFIG_PATH="${selfdir}/lib1"
+	export PKG_CONFIG_SYSROOT_DIR="/sysroot"
+
+	atf_check \
+		-o inline:"-I/sysroot/usr/include \n" \
+		pkgconf --cflags sysroot-dir-2
+
+	atf_check \
+		-o inline:"-I/sysroot/usr/include \n" \
+		pkgconf --cflags sysroot-dir-3
+
+	atf_check \
+		-o inline:"-I/sysroot/usr/include \n" \
+		pkgconf --cflags sysroot-dir-5
+
+	export PKG_CONFIG_SYSROOT_DIR="${SYSROOT_DIR}"
+
+	atf_check \
+		-o inline:"-I${SYSROOT_DIR}/usr/include \n" \
+		pkgconf --cflags sysroot-dir-4
 }
