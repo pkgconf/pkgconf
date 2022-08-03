a46ce3672fab56d6e1ee1c1e493fda62dbf8e13b
Dylan Baker <dylan@pnwbakers.com>
2022-08-03 16:40:04 -0700

    queue: when collecting dependents don't iterate private twice
    Currently, the private field is iterated collecting private deps and
    normal deps. It should only be iterated when collecting private deps.

--- libpkgconf/queue.c
+++ libpkgconf/queue.c
@@ -116,22 +116,27 @@ pkgconf_queue_collect_dependents(pkgconf_client_t *client, pkgconf_pkg_t *pkg, v
 	if (pkg == world)
 		return;
 
-	PKGCONF_FOREACH_LIST_ENTRY(pkg->required.head, node)
+	if (!(pkg->flags & PKGCONF_PKG_PKGF_SEARCH_PRIVATE))
 	{
-		pkgconf_dependency_t *flattened_dep;
+		PKGCONF_FOREACH_LIST_ENTRY(pkg->required.head, node)
+		{
+			pkgconf_dependency_t *flattened_dep;
 
-		flattened_dep = pkgconf_dependency_copy(client, node->data);
+			flattened_dep = pkgconf_dependency_copy(client, node->data);
 
-		pkgconf_node_insert(&flattened_dep->iter, flattened_dep, &world->required);
+			pkgconf_node_insert(&flattened_dep->iter, flattened_dep, &world->required);
+		}
 	}
-
-	PKGCONF_FOREACH_LIST_ENTRY(pkg->requires_private.head, node)
+	else
 	{
-		pkgconf_dependency_t *flattened_dep;
+		PKGCONF_FOREACH_LIST_ENTRY(pkg->requires_private.head, node)
+		{
+			pkgconf_dependency_t *flattened_dep;
 
-		flattened_dep = pkgconf_dependency_copy(client, node->data);
+			flattened_dep = pkgconf_dependency_copy(client, node->data);
 
-		pkgconf_node_insert(&flattened_dep->iter, flattened_dep, &world->requires_private);
+			pkgconf_node_insert(&flattened_dep->iter, flattened_dep, &world->requires_private);
+		}
 	}
 }
 
