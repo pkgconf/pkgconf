de51b03cc1aa023564aa18e3c4cf1b4dc58db27e
Ariadne Conill <ariadne@dereferenced.org>
2023-08-04 08:45:16 +0000

    pkgconf 2.0.0.

--- libpkgconf/libpkgconf.h
+++ libpkgconf/libpkgconf.h
@@ -79,8 +79,8 @@ typedef struct pkgconf_cross_personality_ pkgconf_cross_personality_t;
 #define PKGCONF_FOREACH_LIST_ENTRY_REVERSE(tail, value) \
 	for ((value) = (tail); (value) != NULL; (value) = (value)->prev)
 
-#define LIBPKGCONF_VERSION	10905
-#define LIBPKGCONF_VERSION_STR	"1.9.5"
+#define LIBPKGCONF_VERSION	20000
+#define LIBPKGCONF_VERSION_STR	"2.0.0"
 
 struct pkgconf_fragment_ {
 	pkgconf_node_t iter;
