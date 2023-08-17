eaa4bb44a00e65be8014a3f532bf91c5b7898c81
Ariadne Conill <ariadne@dereferenced.org>
2023-08-17 11:56:08 -0700

    pkgconf 2.0.2.

--- libpkgconf/libpkgconf.h
+++ libpkgconf/libpkgconf.h
@@ -80,8 +80,8 @@ typedef struct pkgconf_queue_ pkgconf_queue_t;
 #define PKGCONF_FOREACH_LIST_ENTRY_REVERSE(tail, value) \
 	for ((value) = (tail); (value) != NULL; (value) = (value)->prev)
 
-#define LIBPKGCONF_VERSION	20001
-#define LIBPKGCONF_VERSION_STR	"2.0.1"
+#define LIBPKGCONF_VERSION	20002
+#define LIBPKGCONF_VERSION_STR	"2.0.2"
 
 struct pkgconf_queue_ {
 	pkgconf_node_t iter;
