pkgconf/pkgconf

commit 008d706958c3a2b1d96d5609d362b177ea57dda3

libpkgconf: personality: default: set want_default_static and want_default_pure to true on windows

libpkgconf/personality.c
@@ -24,6 +24,10 @@
 static bool default_personality_init = false;
 static pkgconf_cross_personality_t default_personality = {
 	.name = "default",
+#ifdef _WIN32
+	.want_default_static = true,
+	.want_default_pure = true,
+#endif
 };
 
 static inline void

commit 444846dd2dfc9e3a5ea4904826c1c7a4b9b5589b

personality: drop want_default_pure on windows
We still retain want_default_static to satisfy the requirements of the PE/COFF
linking model.

Fixes: 008d7069 ("libpkgconf: personality: default: set want_default_static and want_default_pure to true on windows")
Signed-off-by: Ariadne Conill <ariadne@dereferenced.org>
Closes: #364

libpkgconf/personality.c
@@ -38,8 +38,13 @@ static unsigned default_personality_init = 0;
 static pkgconf_cross_personality_t default_personality = {
 	.name = "default",
 #ifdef _WIN32
+	/* PE/COFF uses a different linking model than ELF and Mach-O, where
+	 * all transitive dependency references must be processed by the linker
+	 * when linking the final executable image, even if those dependencies
+	 * are pulled in as DLLs.
+	 * This translates to always using --static on Windows targets.
+	 */
 	.want_default_static = true,
-	.want_default_pure = true,
 #endif
 };
 