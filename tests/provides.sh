039e596439f93be59a2266b93ae8b572b55d1b71
Ariadne Conill <ariadne@ariadne.space>
2025-12-29 22:08:25 -0800

    tests: port some provides tests out of kyua
    Signed-off-by: Ariadne Conill <ariadne@ariadne.space>

--- tests/provides.sh
+++ tests/provides.sh
@@ -3,69 +3,11 @@
 . $(atf_get_srcdir)/test_env.sh
 
 tests_init \
-	simple \
-	foo \
 	bar \
 	baz \
 	quux \
 	moo \
-	meow \
-	indirect_dependency_node
-
-simple_body()
-{
-	export PKG_CONFIG_PATH="${selfdir}/lib1"
-OUTPUT="provides-test-foo = 1.0.0
-provides-test-bar > 1.1.0
-provides-test-baz >= 1.1.0
-provides-test-quux < 1.2.0
-provides-test-moo <= 1.2.0
-provides-test-meow != 1.3.0
-provides = 1.2.3
-"
-	atf_check \
-		-o inline:"${OUTPUT}" \
-		pkgconf --print-provides provides
-	atf_check \
-		-o inline:"-lfoo\n" \
-		pkgconf --libs provides-request-simple
-	atf_check \
-		-e ignore \
-		-s exit:1 \
-		pkgconf --no-provides --libs provides-request-simple
-}
-
-foo_body()
-{
-	export PKG_CONFIG_PATH="${selfdir}/lib1"
-	atf_check \
-		-o ignore \
-		pkgconf --libs provides-test-foo
-	atf_check \
-		-o ignore \
-		pkgconf --libs 'provides-test-foo = 1.0.0'
-	atf_check \
-		-o ignore \
-		pkgconf --libs 'provides-test-foo >= 1.0.0'
-	atf_check \
-		-o ignore \
-		pkgconf --libs 'provides-test-foo <= 1.0.0'
-	atf_check \
-		-s exit:1 \
-		-e ignore \
-		-o ignore \
-		pkgconf --libs 'provides-test-foo != 1.0.0'
-	atf_check \
-		-s exit:1 \
-		-e ignore \
-		-o ignore \
-		pkgconf --libs 'provides-test-foo > 1.0.0'
-	atf_check \
-		-s exit:1 \
-		-e ignore \
-		-o ignore \
-		pkgconf --libs 'provides-test-foo < 1.0.0'
-}
+	meow
 
 bar_body()
 {
@@ -296,15 +238,4 @@ meow_body()
 	atf_check \
 		-o ignore \
 		pkgconf --libs 'provides-test-meow < 1.2.9'
-}
-
-indirect_dependency_node_body()
-{
-	atf_check \
-		-o inline:'1.2.3\n' \
-		pkgconf --with-path="${selfdir}/lib1" --modversion 'provides-test-meow'
-	atf_check \
-		-s exit:1 \
-		-e ignore \
-		pkgconf --with-path="${selfdir}/lib1" --modversion 'provides-test-meow = 1.3.0'
 }
