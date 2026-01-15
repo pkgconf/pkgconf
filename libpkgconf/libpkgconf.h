010fb9f95069a5da5d87c7a8c1fb1c40916d4dc0
Elizabeth Kiara Regina Ashford <elizabeth.jennifer.myers@gmail.com>
2026-01-15 14:40:34 -0800

    buffer: fix undefined behaviour in variable argument function
    When arguments are passed through ..., the C standard mandates that char
    is promoted to int (other types are promoted as well, but that's outside
    the scope of this commit). This happens with any argument passed through
    va_start as well. Therefore, the last argument before variable arguments
    in C must be a promoted type. Otherwise, the behaviour is undefined
    (probably fine on most ABIs, but stricter ABIs might break).
    
    Signed-off-by: Elizabeth Kiara Regina Ashford <elizabeth.jennifer.myers@gmail.com>

--- libpkgconf/libpkgconf.h
+++ libpkgconf/libpkgconf.h
@@ -458,7 +458,7 @@ PKGCONF_API void pkgconf_buffer_trim_byte(pkgconf_buffer_t *buffer);
 PKGCONF_API void pkgconf_buffer_finalize(pkgconf_buffer_t *buffer);
 PKGCONF_API void pkgconf_buffer_fputs(pkgconf_buffer_t *buffer, FILE *out);
 PKGCONF_API void pkgconf_buffer_vjoin(pkgconf_buffer_t *buffer, char delim, va_list va);
-PKGCONF_API void pkgconf_buffer_join(pkgconf_buffer_t *buffer, char delim, ...);
+PKGCONF_API void pkgconf_buffer_join(pkgconf_buffer_t *buffer, int delim, ...);
 PKGCONF_API bool pkgconf_buffer_contains(const pkgconf_buffer_t *haystack, const pkgconf_buffer_t *needle);
 PKGCONF_API bool pkgconf_buffer_contains_byte(const pkgconf_buffer_t *haystack, char needle);
 PKGCONF_API bool pkgconf_buffer_match(const pkgconf_buffer_t *haystack, const pkgconf_buffer_t *needle);
