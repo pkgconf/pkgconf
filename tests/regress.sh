192bc8c92324951c9d18a656ac5415f34b04dccb
Ariadne Conill <ariadne@ariadne.space>
2026-01-18 19:47:09 -0800

    tests: move relevant query building regress tests out of kyua
    Signed-off-by: Ariadne Conill <ariadne@ariadne.space>

--- tests/regress.sh
+++ tests/regress.sh
@@ -3,11 +3,6 @@
 . $(atf_get_srcdir)/test_env.sh
 
 tests_init \
-	modversion_one_word_expression \
-	modversion_two_word_expression \
-	modversion_three_word_expression \
-	modversion_one_word_expression_no_space \
-	modversion_one_word_expression_no_space_zero \
 	explicit_sysroot
 
 #	sysroot_munge \
@@ -26,34 +21,4 @@ explicit_sysroot_body()
 	export PKG_CONFIG_SYSROOT_DIR=${selfdir}
 	atf_check -o inline:"${selfdir}/usr/share/test\n" \
 		pkgconf --with-path="${selfdir}/lib1" --variable=pkgdatadir explicit-sysroot
-}
-
-modversion_one_word_expression_body()
-{
-	atf_check -o inline:"1.2.3\n" \
-		pkgconf --with-path="${selfdir}/lib1" --modversion "foo > 1.0"
-}
-
-modversion_two_word_expression_body()
-{
-	atf_check -o inline:"1.2.3\n" \
-		pkgconf --with-path="${selfdir}/lib1" --modversion foo "> 1.0"
-}
-
-modversion_three_word_expression_body()
-{
-	atf_check -o inline:"1.2.3\n" \
-		pkgconf --with-path="${selfdir}/lib1" --modversion foo ">" 1.0
-}
-
-modversion_one_word_expression_no_space_body()
-{
-	atf_check -o inline:"1.2.3\n" \
-		pkgconf --with-path="${selfdir}/lib1" --modversion "foo >1.0"
-}
-
-modversion_one_word_expression_no_space_zero_body()
-{
-	atf_check -o inline:"1.2.3\n" \
-		pkgconf --with-path="${selfdir}/lib1" --modversion "foo >0.5"
 }
