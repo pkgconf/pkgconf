4b8897235fa3260f0a3b01e1cce8882a2476891a
Ariadne Conill <ariadne@ariadne.space>
2026-03-09 22:36:23 -0700

    libpkgconf: pkg: rework pkg_get_parent_dir to use pkgconf_buffer
    Signed-off-by: Ariadne Conill <ariadne@ariadne.space>

--- libpkgconf/pkg.c
+++ libpkgconf/pkg.c
@@ -52,38 +52,40 @@ str_has_suffix(const char *str, const char *suffix)
 static char *
 pkg_get_parent_dir(pkgconf_pkg_t *pkg)
 {
-	char buf[PKGCONF_ITEM_SIZE], *pathbuf;
+	pkgconf_buffer_t buf = PKGCONF_BUFFER_INITIALIZER;
+	pkgconf_buffer_t pathbuf = PKGCONF_BUFFER_INITIALIZER;
 
-	pkgconf_strlcpy(buf, pkg->filename, sizeof buf);
+	pkgconf_buffer_append(&buf, pkg->filename);
+
 #ifndef _WIN32
-	/*
-	 * We want to resolve symlinks, since ${pcfiledir} should point to the
-	 * parent of the file symlinked to.
-	 */
 	struct stat path_stat;
-	while (!lstat(buf, &path_stat) && S_ISLNK(path_stat.st_mode))
+
+	while (buf.base != NULL &&
+	       !lstat(buf.base, &path_stat) &&
+	       S_ISLNK(path_stat.st_mode))
 	{
 		char sourcebuf[PKGCONF_ITEM_SIZE];
+		char *targetfilename, *targetdir;
 
-		/*
-		 * Have to split the path into the dir + file components,
-		 * in order to extract the directory file descriptor.
-		 *
-		 * The nomenclature here uses the
-		 *
-		 *   ln <source> <target>
-		 *
-		 * model.
-		 */
-		char basenamebuf[PKGCONF_ITEM_SIZE];
-		pkgconf_strlcpy(basenamebuf, buf, sizeof(basenamebuf));
+		pkgconf_buffer_reset(&pathbuf);
+		pkgconf_buffer_append(&pathbuf, buf.base);
 
-		char dirnamebuf[PKGCONF_ITEM_SIZE];
-		pkgconf_strlcpy(dirnamebuf, buf, sizeof(dirnamebuf));
-		const char* targetdir = dirname(dirnamebuf);
+		targetfilename = strrchr(pathbuf.base, '/');
+		if (targetfilename != NULL)
+		{
+			*targetfilename++ = '\0';
+			targetdir = pathbuf.base;
+
+			if (*targetdir == '\0')
+				targetdir = "/";
+		}
+		else
+		{
+			targetfilename = pathbuf.base;
+			targetdir = ".";
+		}
 
 #ifdef HAVE_DECL_READLINKAT
-		const char* targetfilename = basename(basenamebuf);
 		const int dirfd = open(targetdir, O_DIRECTORY);
 		if (dirfd == -1)
 			break;
@@ -91,14 +93,15 @@ pkg_get_parent_dir(pkgconf_pkg_t *pkg)
 		ssize_t len = readlinkat(dirfd, targetfilename, sourcebuf, sizeof(sourcebuf) - 1);
 		close(dirfd);
 #else
-		ssize_t len = readlink(buf, sourcebuf, sizeof(sourcebuf) - 1);
+		ssize_t len = readlink(buf.base, sourcebuf, sizeof(sourcebuf) - 1);
 #endif
 
 		if (len == -1)
 			break;
 		sourcebuf[len] = '\0';
 
-		memset(buf, '\0', sizeof buf);
+		pkgconf_buffer_reset(&buf);
+
 		/*
 		 * The logic here can be a bit tricky, so here's a table:
 		 *
@@ -109,23 +112,25 @@ pkg_get_parent_dir(pkgconf_pkg_t *pkg)
 		 *     /bar (absolute)  |  /foo/link (absolute)  |         /bar (absolute)
 		 *   ../bar (relative)  |  /foo/link (absolute)  |  /foo/../bar (relative)
 		 */
-		if ((sourcebuf[0] != '/')        /* absolute path in <source> wins */
-		    && (strcmp(targetdir, "."))) /* do not prepend "." */
-		{
-			pkgconf_strlcat(buf, targetdir, sizeof buf);
-			pkgconf_strlcat(buf, "/", sizeof buf);
-		}
-		pkgconf_strlcat(buf, sourcebuf, sizeof buf);
+		if ((sourcebuf[0] != '/') && strcmp(targetdir, "."))
+			pkgconf_buffer_append_fmt(&buf, "%s/", targetdir);
+
+		pkgconf_buffer_append(&buf, sourcebuf);
 	}
 #endif
 
-	pathbuf = strrchr(buf, PKG_DIR_SEP_S);
-	if (pathbuf == NULL)
-		pathbuf = strrchr(buf, '/');
-	if (pathbuf != NULL)
-		pathbuf[0] = '\0';
+	pkgconf_buffer_finalize(&pathbuf);
 
-	return strdup(buf);
+	if (pkgconf_buffer_len(&buf) > 0)
+	{
+		char *pathbufp = strrchr(buf.base, PKG_DIR_SEP_S);
+		if (pathbufp == NULL)
+			pathbufp = strrchr(buf.base, '/');
+		if (pathbufp != NULL)
+			pathbufp[0] = '\0';
+	}
+
+	return pkgconf_buffer_freeze(&buf);
 }
 
 typedef void (*pkgconf_pkg_parser_keyword_func_t)(pkgconf_client_t *client, pkgconf_pkg_t *pkg, const char *keyword, const char *warnprefix, const ptrdiff_t offset, const char *value);
