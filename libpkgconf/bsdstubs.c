392029cf1665246b7eab8cc7022e4d46c75c9d0d
Ariadne Conill <ariadne@ariadne.space>
2026-03-09 23:16:50 -0700

    libpkgconf: bsdstubs: remove strlcpy stub
    Signed-off-by: Ariadne Conill <ariadne@ariadne.space>

--- libpkgconf/bsdstubs.c
+++ libpkgconf/bsdstubs.c
@@ -1,60 +1,5 @@
-/*	$OpenBSD: strlcpy.c,v 1.10 2005/08/08 08:05:37 espie Exp $	*/
-
 /*
- * Copyright (c) 1998 Todd C. Miller <Todd.Miller@courtesan.com>
- *
- * Permission to use, copy, modify, and distribute this software for any
- * purpose with or without fee is hereby granted, provided that the above
- * copyright notice and this permission notice appear in all copies.
- *
- * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
- * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
- * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
- * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
- * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
- * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
- * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
- */
-
-#include <libpkgconf/stdinc.h>
-#include <libpkgconf/bsdstubs.h>
-#include <libpkgconf/config.h>
-
-#if !HAVE_DECL_STRLCPY
-/*
- * Copy src to string dst of size siz.  At most siz-1 characters
- * will be copied.  Always NUL terminates (unless siz == 0).
- * Returns strlen(src); if retval >= siz, truncation occurred.
- */
-static inline size_t
-strlcpy(char *dst, const char *src, size_t siz)
-{
-	char *d = dst;
-	const char *s = src;
-	size_t n = siz;
-
-	/* Copy as many bytes as will fit */
-	if (n != 0) {
-		while (--n != 0) {
-			if ((*d++ = *s++) == '\0')
-				break;
-		}
-	}
-
-	/* Not enough room in dst, add NUL and traverse rest of src */
-	if (n == 0) {
-		if (siz != 0)
-			*d = '\0';		/* NUL-terminate dst */
-		while (*s++)
-			;
-	}
-
-	return(s - src - 1);	/* count does not include NUL */
-}
-#endif
-
-/*
- * Copyright (c) 2012 William Pitcock <nenolod@dereferenced.org>.
+ * Copyright (c) 2012 Ariadne Conill <ariadne@dereferenced.org>
  *
  * Permission to use, copy, modify, and/or distribute this software for any
  * purpose with or without fee is hereby granted, provided that the above
@@ -64,6 +9,10 @@ strlcpy(char *dst, const char *src, size_t siz)
  * implied.  In no event shall the authors be liable for any damages arising
  * from the use of this software.
  */
+
+#include <libpkgconf/stdinc.h>
+#include <libpkgconf/bsdstubs.h>
+#include <libpkgconf/config.h>
 
 #if !HAVE_DECL_STRNDUP
 /*
@@ -100,12 +49,6 @@ unveil(const char *path, const char *permissions)
 	return 0;
 }
 #endif
-
-size_t
-pkgconf_strlcpy(char *dst, const char *src, size_t siz)
-{
-	return strlcpy(dst, src, siz);
-}
 
 char *
 pkgconf_strndup(const char *src, size_t len)
