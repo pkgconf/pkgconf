f5edc57327ee128602710c529c1041301532b2d3
Elizabeth Kiara Regina Ashford <elizabeth.jennifer.myers@gmail.com>
2026-03-27 20:39:54 -0700

    pkgconf: Implement Cflags.shared
    Signed-off-by: Elizabeth Kiara Regina Ashford <elizabeth.jennifer.myers@gmail.com>

--- libpkgconf/libpkgconf.h
+++ libpkgconf/libpkgconf.h
@@ -220,6 +220,7 @@ struct pkgconf_pkg_ {
 	pkgconf_list_t libs_private;
 	pkgconf_list_t cflags;
 	pkgconf_list_t cflags_private;
+	pkgconf_list_t cflags_shared;
 
 	pkgconf_list_t required;		/* this used to be requires but that is now a reserved keyword */
 	pkgconf_list_t requires_private;
