cf3c5dc59db4260b9d67d58542cf2863363532ef
Ariadne Conill <ariadne@ariadne.space>
2026-06-24 18:17:42 -0700

    pkgconf 2.9.91.
    Signed-off-by: Ariadne Conill <ariadne@ariadne.space>

--- libpkgconf/libpkgconf.h
+++ libpkgconf/libpkgconf.h
@@ -71,8 +71,8 @@ typedef struct pkgconf_license_ pkgconf_license_t;
 #define PKGCONF_FOREACH_LIST_ENTRY_REVERSE(tail, value) \
 	for ((value) = (tail); (value) != NULL; (value) = (value)->prev)
 
-#define LIBPKGCONF_VERSION	20501
-#define LIBPKGCONF_VERSION_STR	"2.5.1"
+#define LIBPKGCONF_VERSION	20991
+#define LIBPKGCONF_VERSION_STR	"2.9.91"
 
 struct pkgconf_queue_ {
 	pkgconf_node_t iter;
