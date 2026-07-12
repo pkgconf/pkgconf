4b41ca92da14c2c9b3ccd22c0accb3df67873d3e
Ariadne Conill <ariadne@ariadne.space>
2026-07-12 00:48:58 -0700

    pkgconf 3.0.1.
    Signed-off-by: Ariadne Conill <ariadne@ariadne.space>

--- libpkgconf/libpkgconf.h
+++ libpkgconf/libpkgconf.h
@@ -71,8 +71,8 @@ typedef struct pkgconf_license_ pkgconf_license_t;
 #define PKGCONF_FOREACH_LIST_ENTRY_REVERSE(tail, value) \
 	for ((value) = (tail); (value) != NULL; (value) = (value)->prev)
 
-#define LIBPKGCONF_VERSION	30000
-#define LIBPKGCONF_VERSION_STR	"3.0.0"
+#define LIBPKGCONF_VERSION	30001
+#define LIBPKGCONF_VERSION_STR	"3.0.1"
 
 struct pkgconf_queue_ {
 	pkgconf_node_t iter;
