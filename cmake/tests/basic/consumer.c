a5b79778406e98db37d5502b2f55f2844d373b4b
Elizabeth Kiara Regina Ashford <elizabeth.jennifer.myers@gmail.com>
2026-07-13 17:02:01 -0700

    cmake: reformat C tests
    Signed-off-by: Elizabeth Kiara Regina Ashford <elizabeth.jennifer.myers@gmail.com>

--- cmake/tests/basic/consumer.c
+++ cmake/tests/basic/consumer.c
@@ -5,6 +5,6 @@ int
 main(void)
 {
     int cmp = pkgconf_compare_version("1.0", "1.0");
-	  printf("pkgconf_compare_version(\"1.0\", \"1.0\") = %d\n", cmp);
-	  return cmp == 0 ? 0 : 1;
+    printf("pkgconf_compare_version(\"1.0\", \"1.0\") = %d\n", cmp);
+    return cmp == 0 ? 0 : 1;
 }
